f74c434e1a0ea668e2eaefd33a210d58f4d7e941
Luke Jacobsen <lukejacobsen@gmail.com>
2025-06-27 16:12:33 +0200

    fix margins

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,7 +2,7 @@ import { Container, Typography } from '@mui/material'
 import { createFileRoute } from '@tanstack/react-router'
 
 const Index = () => (
-  <Container disableGutters sx={{ pt: 4, mx: 4 }}>
+  <Container disableGutters sx={{ p: ({ spacing }) => spacing(4) }}>
     <Typography color='secondary' variant='h3'>Welcome to the Lumos home page!</Typography>
   </Container>
 )
--- src/routes/prices.tsx
+++ src/routes/prices.tsx
@@ -2,7 +2,7 @@ import { Container, Typography } from '@mui/material'
 import { createFileRoute } from '@tanstack/react-router'
 
 const Prices = () => {
-    return <Container disableGutters sx={{ pt: 4, mx: 4 }}>
+    return <Container disableGutters sx={{ p: ({ spacing }) => spacing(4) }}>
         <Typography color='secondary' variant='h3'>Check out our prices!</Typography>
     </Container>
 }
