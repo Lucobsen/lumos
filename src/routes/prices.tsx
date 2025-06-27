40913bfad7bf7ee2e07bb0a3167be2beeb2f9122
Luke Jacobsen <lukejacobsen@gmail.com>
2025-06-27 16:14:32 +0200

    fix margin again

--- src/routes/prices.tsx
+++ src/routes/prices.tsx
@@ -2,7 +2,7 @@ import { Container, Typography } from '@mui/material'
 import { createFileRoute } from '@tanstack/react-router'
 
 const Prices = () => {
-    return <Container disableGutters sx={{ p: ({ spacing }) => spacing(4) }}>
+    return <Container disableGutters sx={{ p: ({ spacing }) => spacing(4), m: 0 }}>
         <Typography color='secondary' variant='h3'>Check out our prices!</Typography>
     </Container>
 }
