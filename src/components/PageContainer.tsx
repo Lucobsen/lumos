e30b17ac2aaad6c71b12b1a0d0eb429d47c0dbe8
Luke Jacobsen <lukejacobsen@gmail.com>
2025-06-30 19:45:46 +0200

    add basic contact info

--- src/components/PageContainer.tsx
+++ src/components/PageContainer.tsx
@@ -5,7 +5,17 @@ export const PageContainer = ({ children }: { children: React.ReactNode }) => {
     const isSmallScreen = useMediaQuery(breakpoints.down(900));
 
     return (
-        <Container maxWidth={false} disableGutters sx={{ p: ({ spacing }) => isSmallScreen ? spacing(4) : spacing(4, 8), m: ({ spacing }) => spacing(isSmallScreen ? 12.5 : 15, 0, 0, 0) }} >
+        <Container
+            maxWidth={false}
+            disableGutters
+            sx={{
+                p: ({ spacing }) => isSmallScreen ? spacing(4) : spacing(4, 8),
+                m: ({ spacing }) => spacing(isSmallScreen ? 12.5 : 15, 0, 0, 0),
+                alignItems: 'center',
+                display: 'flex',
+                flexDirection: 'column',
+            }}
+        >
             {children}
         </Container>);
 };
