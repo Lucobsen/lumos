dca64f652d4a35139b1fd1012a4a1e091d71dffa
Luke Jacobsen <lukejacobsen@gmail.com>
2025-07-13 15:49:38 +0200

    add more pricing info

--- src/components/PriceItem.tsx
+++ src/components/PriceItem.tsx
@@ -1,16 +1,17 @@
 import { Box, List, ListItem, Stack, SvgIcon, Typography, useMediaQuery, useTheme } from '@mui/material';
 import HorizontalRuleIcon from '@mui/icons-material/HorizontalRule';
 
-type PriceItemProps = { title: string, infoItems: string[] };
+type PriceItemProps = { title: string, infoItems: string[], cost: string };
 
-export const PriceItem = ({ title, infoItems }: PriceItemProps) => {
+export const PriceItem = ({ title, infoItems, cost }: PriceItemProps) => {
     const { breakpoints } = useTheme();
     const isSmallScreen = useMediaQuery(breakpoints.down(1000));
 
     return (
-        <Stack border={2} borderRadius={15} borderColor='#000' height='fit-content' pb={2}>
+        <Stack boxShadow={({ shadows }) => shadows[8]} border={2} borderRadius={15} borderColor='#000' height='fit-content' pb={2}>
             <Box textAlign='center' bgcolor='primary.light' borderBottom='2px solid #000' p={2} sx={{ borderTopLeftRadius: '58px', borderTopRightRadius: '58px' }}>
                 <Typography color='secondary' variant={isSmallScreen ? 'h5' : 'h3'}>{title}</Typography>
+                <Typography color='secondary' variant={isSmallScreen ? 'h6' : 'h4'}>{cost}</Typography>
             </Box>
             <List>
                 {infoItems.map(info =>
